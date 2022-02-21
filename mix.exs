6dcf8014690187a8ca2e9ec5753f360a44d15dc6
Tomasz Tomczyk <me@tomasztomczyk.com>
2022-02-21 23:29:40 +0000

    Lowercase app name

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Recur.MixProject do
 
   def project do
     [
-      app: :Recur,
+      app: :recur,
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
